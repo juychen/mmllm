e4f76e22cdb5e84175d1850d53a1147efa6796b8
junyichen <winghare@hotmail.com>
2026-04-15 17:01:11 +0800

    Add more test case

--- test_run.sh
+++ test_run.sh
@@ -4,7 +4,6 @@ conda activate evo2
 cd /home/junyichen/code/mmllm/
 # GET current time
 current_time=$(date "+%Y-%m-%d-%H-%M-%S")
-echo "Current time: $current_time"
 # python run_sample_size_experiments.py \
 #   --sample-sizes 50000\
 #   --use-sequence --use-atac \
@@ -26,16 +25,21 @@ echo "Current time: $current_time"
 #   --scheduler-min-lr 1e-5 \
 #   --output-csv output/$current_time\_sample_size_results.csv \
 #   --output-json output/$current_time\_sample_size_results.json
-# python run_sample_size_experiments.py \
-#   --sample-sizes 50000\
-#   --no-use-sequence --use-atac \
-#   --scheduler cosine \
-#   --num-epochs 100 \
-#   --batch-size 64 \
-#   --scheduler-patience 5 \
-#   --scheduler-min-lr 1e-5 \
-#   --output-csv output/$current_time\_sample_size_results.csv \
-#   --output-json output/$current_time\_sample_size_results.json
+echo "Current time: $current_time"
+python run_sample_size_experiments.py \
+  --sample-sizes 2000 20000 100000\
+  --use-sequence --no-use-atac \
+  --scheduler cosine \
+  --num-epochs 100 \
+  --batch-size 64 \
+  --scheduler-patience 5 \
+  --timestamp "$current_time" \
+  --scheduler-min-lr 1e-5 \
+  --output-csv output/$current_time\_mc_hmc_results.csv \
+  --output-json output/$current_time\_mc_hmc_results.json \
+  --prediction-signal-csv output/$current_time\_prediction_mchmc_{sample_size}.csv \
+  --regression-plot-path output/$current_time\_regression_mchmc_{sample_size}.png
+
 current_time=$(date "+%Y-%m-%d-%H-%M-%S")
 python run_atac_query_sequence_context_experiments.py \
   --sample-sizes 2000 20000 100000\
@@ -46,10 +50,11 @@ python run_atac_query_sequence_context_experiments.py \
   --scheduler-patience 5 \
   --scheduler-min-lr 1e-5 \
   --timestamp "$current_time" \
-  --output-csv output/$current_time\_sample_atac_results.csv \
-  --output-json output/$current_time\_sample_atac_results.json \
-  --prediction-signal-csv output/$current_time\_prediction_signals_{sample_size}.csv \
-  --regression-plot-path output/$current_time\_regression_plot_{sample_size}.png
+  --output-csv output/$current_time\_atac_5mc_results.csv \
+  --output-json output/$current_time\_atac_5mc_results.json \
+  --prediction-signal-csv output/$current_time\_prediction_5mc_{sample_size}.csv \
+  --regression-plot-path output/$current_time\_regression_5mc_{sample_size}.png
+
 current_time=$(date "+%Y-%m-%d-%H-%M-%S")
 python run_atac_query_sequence_context_experiments.py \
   --sample-sizes 2000 20000 100000\
@@ -60,7 +65,7 @@ python run_atac_query_sequence_context_experiments.py \
   --scheduler-patience 5 \
   --scheduler-min-lr 1e-5 \
   --timestamp "$current_time" \
-  --output-csv output/$current_time\_sample_atac_results.csv \
-  --output-json output/$current_time\_sample_atac_results.json \
-  --prediction-signal-csv output/$current_time\_prediction_signals_{sample_size}.csv \
-  --regression-plot-path output/$current_time\_regression_plot_{sample_size}.png \
+  --output-csv output/$current_time\_atac_5h_results.csv \
+  --output-json output/$current_time\_atac_5h_results.json \
+  --prediction-signal-csv output/$current_time\_prediction_5h_{sample_size}.csv \
+  --regression-plot-path output/$current_time\_regression_5h_{sample_size}.png \
