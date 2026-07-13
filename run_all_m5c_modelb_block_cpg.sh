53520111236aed09667cbdec488803807ad70efc
junyichen <winghare@hotmail.com>
2026-07-13 16:20:30 +0800

    AD

--- run_all_m5c_modelb_block_cpg.sh
+++ run_all_m5c_modelb_block_cpg.sh
@@ -15,8 +15,8 @@ if [[ "$fusion_type" != "cross_hyena" && "$fusion_type" != "cross_attention" ]];
   exit 1
 fi
 
-regions=("PFC")
-conditions=("MW")
+regions=("AMY")
+conditions=("MC")
 
 run_experiment() {
   local region="$1"
