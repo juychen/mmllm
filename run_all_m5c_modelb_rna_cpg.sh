6b95b65852a2fc1ccafc8bcc20ed86eaec1257ce
junyichen <winghare@hotmail.com>
2026-09-09 15:53:18 +0800

    commitbefore masked learning

--- run_all_m5c_modelb_rna_cpg.sh
+++ run_all_m5c_modelb_rna_cpg.sh
@@ -15,8 +15,8 @@ if [[ "$fusion_type" != "cross_hyena" && "$fusion_type" != "cross_attention" ]];
   exit 1
 fi
 
-regions=("AMY")
-conditions=("MW")
+regions=("HIP")
+conditions=("MW" "MC")
 
 run_experiment() {
   local region="$1"
